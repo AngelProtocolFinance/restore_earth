215b4ffae93da884999a793dd20e874d4c91c346
David Lumley <david@davidlumley.xyz>
2021-11-21 22:03:14 -0800

    add suggested donations

--- pages/donate.tsx
+++ pages/donate.tsx
@@ -109,8 +109,15 @@ const Connect = ({
 };
 
 const TOKENS = {
+  BITCOIN: "BTC",
   ETHEREUM: "ETH",
   TERRA: "UST",
+};
+
+const SUGGESTED_DONATION_AMOUNTS = {
+  BITCOIN: ["0.0017", "0.0087", "0.017", "0.087", "0.44", "0.87", "1.74"],
+  ETHEREUM: ["0.024", "0.12", "0.24", "1.2", "6", "12", "24"],
+  TERRA: ["100", "500", "1000", "5000", "25000", "50000", "100000"],
 };
 
 const Donate = ({ setStep, wallet, onDonate }) => {
@@ -118,6 +125,7 @@ const Donate = ({ setStep, wallet, onDonate }) => {
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [amount, setAmount] = useState("");
+  const [selectedAmount, setSelectedAmount] = useState("");
   const [receiveNft, setReceiveNft] = useState(false);
   const [receiveReceipt, setReceiveReceipt] = useState(false);
   const [tcaMember, setTcaMember] = useState(false);
@@ -177,6 +185,7 @@ const Donate = ({ setStep, wallet, onDonate }) => {
 
   const address = wallet.methods.address();
   const token = TOKENS[wallet.chain];
+  const suggestedDonationAmounts = SUGGESTED_DONATION_AMOUNTS[wallet.chain];
 
   const onSuccess = () => {
     setStep(steps.THANKYOU);
@@ -188,6 +197,10 @@ const Donate = ({ setStep, wallet, onDonate }) => {
 
   const onClickDisconnect = () => {
     wallet.methods.disconnect();
+  };
+
+  const onClickReset = () => {
+    onClickDisconnect();
   };
 
   const onSubmit = (e) => {
@@ -207,14 +220,12 @@ const Donate = ({ setStep, wallet, onDonate }) => {
             setPendingRequest(false);
             setError(true);
             setErrorMessage(error);
-            // console.log("error donating to wallet: ", error);
           });
       })
       .catch((error) => {
         setPendingRequest(false);
         setError(true);
         setErrorMessage(error);
-        console.log("error posting kyc data: ", error);
       });
   };
 
@@ -226,16 +237,48 @@ const Donate = ({ setStep, wallet, onDonate }) => {
       <Form onSubmit={onSubmit}>
         <Form.Group className="mb-3" controlId="formAmount">
           <Form.Label>Amount</Form.Label>
-          <InputGroup>
-            <InputGroup.Text>{token}</InputGroup.Text>
-            <Form.Control
-              type="text"
-              name="amount"
-              value={amount}
-              onChange={(e) => setAmount(e.currentTarget.value)}
-              className="donate__form__address_input"
+          {suggestedDonationAmounts.map((suggestedAmount) => {
+            return (
+              <Form.Check type="radio" key={suggestedAmount}>
+                <Form.Check.Input
+                  type="radio"
+                  name="formAmount"
+                  onChange={(e) => {
+                    const selectedAmount = e.currentTarget.value;
+                    setSelectedAmount(selectedAmount);
+                    setAmount(selectedAmount);
+                  }}
+                  value={suggestedAmount}
+                  checked={selectedAmount == suggestedAmount}
+                />
+                <Form.Check.Label>
+                  {suggestedAmount} {token}
+                </Form.Check.Label>
+              </Form.Check>
+            );
+          })}
+          <Form.Check type="radio" id={`formAmount-custom`}>
+            <Form.Check.Input
+              type="radio"
+              name="formAmount"
+              onChange={(e) => {
+                setSelectedAmount("custom");
+              }}
             />
-          </InputGroup>
+            <Form.Check.Label>
+              <Form.Control
+                type="text"
+                name="amount"
+                value={amount}
+                onFocus={(e) => {
+                  setAmount(e.currentTarget.value);
+                  document.getElementById("formAmount-custom").click();
+                }}
+                onChange={(e) => setAmount(e.currentTarget.value)}
+                className="donate__form__amount__input"
+              />
+            </Form.Check.Label>
+          </Form.Check>
           <Form.Text className="text-muted">
             The amount you want to donate
           </Form.Text>
@@ -265,7 +308,7 @@ const Donate = ({ setStep, wallet, onDonate }) => {
                     const address = e.currentTarget.value;
                     onChangeNftData({ address });
                   }}
-                  className="donate__form__address_input"
+                  className="donate__form__nft__input"
                 />
               </InputGroup>
               <Form.Text className="text-muted">
@@ -303,7 +346,7 @@ const Donate = ({ setStep, wallet, onDonate }) => {
                   const name = e.currentTarget.value;
                   onChangeKycData({ name });
                 }}
-                className="donate__form__address_input"
+                className="donate__form__kyc__input"
               />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formEmail">
@@ -316,7 +359,7 @@ const Donate = ({ setStep, wallet, onDonate }) => {
                   const email = e.currentTarget.value;
                   onChangeKycData({ email });
                 }}
-                className="donate__form__address_input"
+                className="donate__form__kyc__input"
               />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formEmail">
@@ -330,7 +373,7 @@ const Donate = ({ setStep, wallet, onDonate }) => {
                       const streetAddress = e.currentTarget.value;
                       onChangeKycData({ streetAddress });
                     }}
-                    className="donate__form__address_input"
+                    className="donate__form__kyc__input"
                   />
                 </Col>
               </Row>
@@ -343,7 +386,7 @@ const Donate = ({ setStep, wallet, onDonate }) => {
                       const city = e.currentTarget.value;
                       onChangeKycData({ city });
                     }}
-                    className="donate__form__address_input"
+                    className="donate__form__kyc__input"
                   />
                 </Col>
                 <Col>
@@ -354,7 +397,7 @@ const Donate = ({ setStep, wallet, onDonate }) => {
                       const state = e.currentTarget.value;
                       onChangeKycData({ state });
                     }}
-                    className="donate__form__address_input"
+                    className="donate__form__kyc__input"
                   />
                 </Col>
               </Row>
@@ -367,7 +410,7 @@ const Donate = ({ setStep, wallet, onDonate }) => {
                       const country = e.currentTarget.value;
                       onChangeKycData({ country });
                     }}
-                    className="donate__form__address_input"
+                    className="donate__form__kyc__input"
                   />
                 </Col>
                 <Col>
@@ -378,7 +421,7 @@ const Donate = ({ setStep, wallet, onDonate }) => {
                       const zipcode = e.currentTarget.value;
                       onChangeKycData({ zipcode });
                     }}
-                    className="donate__form__address_input"
+                    className="donate__form__kyc__input"
                   />
                 </Col>
               </Row>
@@ -403,6 +446,7 @@ const Donate = ({ setStep, wallet, onDonate }) => {
               const affiliateId = e.currentTarget.value;
               onChangeTcaData({ affiliateId });
             }}
+            className="donate__form__tca__input"
           >
             <option>Open this select menu</option>
             <option value="1">One</option>
