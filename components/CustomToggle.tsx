4b0151a8c4717e560c254b5fcc2aef4200325ff1
David Lumley <david@davidlumley.xyz>
2021-11-27 04:26:39 -0800

    fix customtoggle colors

--- components/CustomToggle.tsx
+++ components/CustomToggle.tsx
@@ -5,9 +5,10 @@ const CustomToggle = ({ children, eventKey }) => {
   const { activeEventKey } = useContext(AccordionContext);
   const isCurrentEventKey = activeEventKey === eventKey;
   const decoratedOnClick = useAccordionButton(eventKey, () => {});
-  let classNames = "btn btn-block px-12 w-100 btn-outline-dark border-top";
+  let classNames = "btn btn-block px-12 w-100 btn-outline-light border-top";
   if (isCurrentEventKey) {
-    classNames = "btn btn-block px-12 w-100 btn-primary border-top bg-opacity";
+    classNames =
+      "btn btn-block px-12 w-100 btn-outline-primary border-top bg-opacity";
   }
   return (
     <button type="button" className={classNames} onClick={decoratedOnClick}>
